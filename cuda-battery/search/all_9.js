9373c9e13a2d46cdb84f6d1586a1525ab954be6a
Pierre Talbot <pierre.talbot@uni.lu>
2024-08-28 11:04:44 +0200

    Update documentation of cuda-battery-v1.6.0.

--- cuda-battery/search/all_9.js
+++ cuda-battery/search/all_9.js
@@ -3,11 +3,11 @@ var searchData=
   ['make_5froot_0',['make_root',['../namespacebattery.html#ad7b3cdf8a15ca157faba5b4a19cd482e',1,'battery']]],
   ['make_5fshared_1',['make_shared',['../namespacebattery.html#a80acf3e6520c21d3d7fa56171591983e',1,'battery']]],
   ['make_5funique_2',['make_unique',['../namespacebattery.html#a1652654e798862763b7c3461da8b039b',1,'battery']]],
-  ['max_3',['max',['../namespacebattery.html#a0eee188af543e83f604a83eb4224872f',1,'battery']]],
+  ['max_3',['max',['../namespacebattery.html#a400d773016494f4681eab0c2dbd6eadc',1,'battery']]],
   ['memory_4',['memory',['../classbattery_1_1memory.html',1,'battery']]],
   ['memory_2ehpp_5',['memory.hpp',['../memory_8hpp.html',1,'']]],
   ['memory_5ftype_6',['memory_type',['../classbattery_1_1bitset.html#aa03280b3fcb94c70c6442ad9ade491ea',1,'battery::bitset::memory_type'],['../classbattery_1_1dynamic__bitset.html#a410909ba81ed80d99af27ffcef99a681',1,'battery::dynamic_bitset::memory_type']]],
-  ['min_7',['min',['../namespacebattery.html#a02f2e0da017e4d2389b0322b07e26e4c',1,'battery']]],
+  ['min_7',['min',['../namespacebattery.html#a375b99e48991c4ae20bd3804e92ef160',1,'battery']]],
   ['mul_5fdown_8',['mul_down',['../namespacebattery.html#a076fedaed8fce45a5c564872de4db0b5',1,'battery']]],
   ['mul_5fup_9',['mul_up',['../namespacebattery.html#a98d96cfd1ace3bf496862506cc3c4e9f',1,'battery']]]
 ];
