cb9ef3997a728df50f778c16572178c86bd51379
Pierre Talbot <pierre.talbot@uni.lu>
2025-03-21 04:28:45 +0100

    Update documentation of lala-parsing-v1.2.7.

--- lala-parsing/classlala_1_1_solver_output.js
+++ lala-parsing/classlala_1_1_solver_output.js
@@ -9,7 +9,7 @@ var classlala_1_1_solver_output =
     [ "operator=", "classlala_1_1_solver_output.html#a4b928ba3e31764a81db6802db4a0d9b4", null ],
     [ "add_array_var", "classlala_1_1_solver_output.html#ac2bed79c2dc9c5b707f376cf634465ed", null ],
     [ "add_var", "classlala_1_1_solver_output.html#a14b12c754311acc177ebda0bac551ebd", null ],
-    [ "print_solution_flat_zinc", "classlala_1_1_solver_output.html#ac8171c1e27e3454bfce9fe5254021fd2", null ],
+    [ "print_solution_flatzinc", "classlala_1_1_solver_output.html#a8ab92d2b0a52f6f9d1d389f393dacb24", null ],
     [ "print_solution_xml", "classlala_1_1_solver_output.html#a8c64698ef49350cf1ad8ff8e859a4983", null ],
     [ "print_solution", "classlala_1_1_solver_output.html#a5fc957f44516229491594a215f8b4d1a", null ],
     [ "SolverOutput", "classlala_1_1_solver_output.html#af23e8255c42c841e7f3c73a013d1db12", null ]
--- lala-parsing/search/functions_7.js
+++ lala-parsing/search/functions_7.js
@@ -5,6 +5,6 @@ var searchData=
   ['parse_5fxcsp3_173',['parse_xcsp3',['../namespacelala.html#a3a97c11844de386403a70c79b3f5516f',1,'lala::parse_xcsp3(const std::string &amp;filename, XCSP3Core::XCSP3_turbo_callbacks&lt; Allocator &gt; &amp;cb)'],['../namespacelala.html#aa9aa323d05351ffd0b4bbe03918573cb',1,'lala::parse_xcsp3(const std::string &amp;filename, SolverOutput&lt; Allocator &gt; &amp;output, TableDecomposition d=TableDecomposition::ELEMENTS)'],['../namespacelala.html#ac4d15d71a00c72ace68a098342d1e186',1,'lala::parse_xcsp3(const std::string &amp;filename, TableDecomposition d=TableDecomposition::ELEMENTS, const Allocator &amp;allocator=Allocator())']]],
   ['parse_5fxcsp3_5fstr_174',['parse_xcsp3_str',['../namespacelala.html#ad9dfa1139ca012c8decbaf4c19d9da60',1,'lala']]],
   ['print_5fsolution_175',['print_solution',['../classlala_1_1_solver_output.html#a5fc957f44516229491594a215f8b4d1a',1,'lala::SolverOutput']]],
-  ['print_5fsolution_5fflat_5fzinc_176',['print_solution_flat_zinc',['../classlala_1_1_solver_output.html#ac8171c1e27e3454bfce9fe5254021fd2',1,'lala::SolverOutput']]],
+  ['print_5fsolution_5fflatzinc_176',['print_solution_flatzinc',['../classlala_1_1_solver_output.html#a8ab92d2b0a52f6f9d1d389f393dacb24',1,'lala::SolverOutput']]],
   ['print_5fsolution_5fxml_177',['print_solution_xml',['../classlala_1_1_solver_output.html#a8c64698ef49350cf1ad8ff8e859a4983',1,'lala::SolverOutput']]]
 ];
